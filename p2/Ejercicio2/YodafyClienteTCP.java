82c119c8550602e14dbe249bfa6f0147951ab0cb
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-03 19:54:35 +0100

    Ejercicio 2 terminado.

---
+++ p2/Ejercicio2/YodafyClienteTCP.java
@@ -0,0 +1,62 @@
+//
+// YodafyServidorIterativo
+// (CC) jjramos, 2012
+//
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class YodafyClienteTCP {
+
+	public static void main(String[] args) {
+		// Nombre del host donde se ejecuta el servidor:
+		String host="localhost";
+		// Puerto en el que espera el servidor:
+		int port=8989;
+		
+		// Socket para la conexión TCP
+		Socket socketServicio=null;
+		
+		try {
+			// Creamos un socket que se conecte a "hist" y "port":	
+			socketServicio = new Socket(host,port);
+		
+			PrintWriter outPrinter = new PrintWriter(socketServicio.getOutputStream(),true);
+			BufferedReader inReader = new BufferedReader(new InputStreamReader(socketServicio.getInputStream()));
+
+			// Si queremos enviar una cadena de caracteres por un OutputStream, hay que pasarla primero
+			// a un array de bytes:
+			String cadena ="Al monte del volcán debes ir sin demora";
+			
+			// Enviamos el array por el outputStream
+			outPrinter.println(cadena);
+
+			// Podemos usar "flush()" para obligar a TCP a que no espere para hacer el envío:
+			outPrinter.flush();
+
+			// Leemos la respuesta del servidor. Para ello le pasamos un array de bytes, que intentará
+			// rellenar. El método "read(...)" devolverá el número de bytes leídos.			
+			String cadena_recibida = "";
+			cadena_recibida = inReader.readLine();
+
+			// MOstremos la cadena de caracteres recibidos:
+			System.out.println("Recibido: ");
+			System.out.println(cadena_recibida);
+			
+			// Una vez terminado el servicio, cerramos el socket (automáticamente se cierran
+			// el inpuStream  y el outputStream)
+			socketServicio.close();
+
+			// Excepciones:
+		} catch (UnknownHostException e) {
+			System.err.println("Error: Nombre de host no encontrado.");
+		} catch (IOException e) {
+			System.err.println("Error de entrada/salida al abrir el socket.");
+		}
+	}
+}
