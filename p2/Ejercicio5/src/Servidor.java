187038a2631165db51e81781b3da61b7538bdde8
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-07 23:41:26 +0100

    corrige sangrado

--- p2/Ejercicio5/src/Servidor.java
+++ p2/Ejercicio5/src/Servidor.java
@@ -5,38 +5,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
+    public static void main(String[] args) {
+        // Puerto de escucha
+        int port=8989;
+        ServerSocket socketServicio;
+        Socket socketConexion = null;
+        try {
+            // Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
+            socketServicio = new ServerSocket(port);
 
+            do {
 
+                // Aceptamos una nueva conexión con accept()
+                socketConexion = socketServicio.accept();
 
+                // Creamos un objeto de la clase ProcesadorYodafy, pasándole como
+                // argumento el nuevo socket, para que realice el procesamiento
+                // Este esquema permite que se puedan usar hebras más fácilmente.
+                //  ProcesadorYodafy procesador=new ProcesadorYodafy(socketConexion);
+                // procesador.start();
+                Juego juego=new Juego(socketConexion);
+                juego.start();
 
-        public static void main(String[] args) {
-            // Puerto de escucha
-            int port=8989;
-            ServerSocket socketServicio;
-            Socket socketConexion = null;
-            try {
-                // Abrimos el socket en modo pasivo, escuchando el en puerto indicado por "port"
-                socketServicio = new ServerSocket(port);
+            } while (true);
 
-                do {
-
-                    // Aceptamos una nueva conexión con accept()
-                    socketConexion = socketServicio.accept();
-
-                    // Creamos un objeto de la clase ProcesadorYodafy, pasándole como
-                    // argumento el nuevo socket, para que realice el procesamiento
-                    // Este esquema permite que se puedan usar hebras más fácilmente.
-                  //  ProcesadorYodafy procesador=new ProcesadorYodafy(socketConexion);
-                   // procesador.start();
-                    Juego juego=new Juego(socketConexion);
-                    juego.start();
-
-                } while (true);
-
-            } catch (IOException e) {
-                System.err.println("Error al escuchar en el puerto "+port);
-            }
-
+        } catch (IOException e) {
+            System.err.println("Error al escuchar en el puerto "+port);
         }
-
     }
+}
